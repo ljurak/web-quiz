e426d1f693f6af532e44571e2b0d57c2886e3c20
ljurak <lucas.jurak@gmail.com>
2020-08-04 22:00:03 +0200

    Initialize project

---
+++ src/test/java/com/example/webquiz/WebQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
