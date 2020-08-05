f1c0800860a770f5ef264327fc40129e8174729d
ljurak <lucas.jurak@gmail.com>
2020-08-05 23:47:17 +0200

    Add handlers for creating, fetching and solving quizzes

--- src/main/java/com/example/webquiz/controller/QuizController.java
+++ src/main/java/com/example/webquiz/controller/QuizController.java
@@ -1,13 +1,17 @@
 package com.example.webquiz.controller;
 
-import com.example.webquiz.model.Question;
+import com.example.webquiz.model.Quiz;
 import com.example.webquiz.model.Result;
 import com.example.webquiz.service.QuizService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,13 +23,23 @@ public class QuizController {
         this.quizService = quizService;
     }
 
-    @GetMapping("/quiz")
-    public Question getQuiz() {
-        return quizService.getQuiz();
+    @GetMapping("/quizzes/{id}")
+    public Quiz getQuiz(@PathVariable int id) {
+        return quizService.getQuiz(id);
     }
 
-    @PostMapping(path = "/quiz")
-    public Result solveQuiz(@RequestParam int answer) {
-        return quizService.solveQuiz(answer);
+    @GetMapping("/quizzes")
+    public List<Quiz> getAllQuizzes() {
+        return quizService.getAllQuizzes();
+    }
+
+    @PostMapping("/quizzes")
+    public Quiz createQuiz(@RequestBody Quiz quiz) {
+        return quizService.createQuiz(quiz);
+    }
+
+    @PostMapping(path = "/quizzes/{id}/solve")
+    public Result solveQuiz(@PathVariable int id, @RequestParam int answer) {
+        return quizService.solveQuiz(id, answer);
     }
 }
