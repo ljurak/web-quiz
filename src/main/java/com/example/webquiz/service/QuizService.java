cdee1ed3274424b330bac4e308b3d33ef32d150c
ljurak <lucas.jurak@gmail.com>
2020-08-05 23:46:39 +0200

    Add methods for creating, fetching, solving quizzes

--- src/main/java/com/example/webquiz/service/QuizService.java
+++ src/main/java/com/example/webquiz/service/QuizService.java
@@ -1,25 +1,39 @@
 package com.example.webquiz.service;
 
-import com.example.webquiz.model.Question;
+import com.example.webquiz.exception.QuizNotFoundException;
+import com.example.webquiz.model.Quiz;
 import com.example.webquiz.model.Result;
-import com.example.webquiz.repository.QuestionRepo;
+import com.example.webquiz.repository.QuizRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuizService {
 
-    private final QuestionRepo questionRepo;
+    private final QuizRepo quizRepo;
 
-    public QuizService(QuestionRepo questionRepo) {
-        this.questionRepo = questionRepo;
+    public QuizService(QuizRepo quizRepo) {
+        this.quizRepo = quizRepo;
     }
 
-    public Question getQuiz() {
-        return questionRepo.getQuestion();
+    public Quiz getQuiz(int id) {
+        return quizRepo.getQuiz(id)
+                .orElseThrow(() -> new QuizNotFoundException("Quiz not found for id: " + id));
     }
 
-    public Result solveQuiz(int answer) {
-        if (answer == 2) {
+    public List<Quiz> getAllQuizzes() {
+        return quizRepo.getAllQuizzes();
+    }
+
+    public Quiz createQuiz(Quiz quiz) {
+        return quizRepo.createQuiz(quiz);
+    }
+
+    public Result solveQuiz(int quizId, int answer) {
+        Quiz quiz = quizRepo.getQuiz(quizId)
+                .orElseThrow(() -> new QuizNotFoundException("Quiz not found for id: " + quizId));
+        if (quiz.getAnswer() == answer) {
             return new Result(true, "Congratulations, you're right!");
         }
         return new Result(false, "Wrong answer! Please, try again.");
