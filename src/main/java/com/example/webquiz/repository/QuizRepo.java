721993da26799b1f970ac052469cbd1b2e3e291d
ljurak <lucas.jurak@gmail.com>
2020-08-05 23:43:56 +0200

    Add query methods for getting and creating quizzes

--- src/main/java/com/example/webquiz/repository/QuizRepo.java
+++ src/main/java/com/example/webquiz/repository/QuizRepo.java
@@ -3,21 +3,28 @@ package com.example.webquiz.repository;
 import com.example.webquiz.model.Quiz;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
-public class QuestionRepo {
+public class QuizRepo {
 
     private int currentId = 0;
 
     private Map<Integer, Quiz> quizzes = new HashMap<>();
 
-    public Quiz getQuestion(int id) {
-        return quizzes.get(id);
+    public Optional<Quiz> getQuiz(int id) {
+        return Optional.ofNullable(quizzes.get(id));
     }
 
-    public Quiz createQuestion(Quiz quiz) {
+    public List<Quiz> getAllQuizzes() {
+        return new ArrayList<>(quizzes.values());
+    }
+
+    public Quiz createQuiz(Quiz quiz) {
         quiz.setId(++currentId);
         quizzes.put(quiz.getId(), quiz);
         return quiz;
