2f1d1a69eb5bcec00176488fe4e4e5bdbbf26be8
ljurak <lucas.jurak@gmail.com>
2020-08-06 23:51:38 +0200

    Update solveQuiz method

--- src/main/java/com/example/webquiz/service/QuizService.java
+++ src/main/java/com/example/webquiz/service/QuizService.java
@@ -1,11 +1,13 @@
 package com.example.webquiz.service;
 
 import com.example.webquiz.exception.QuizNotFoundException;
+import com.example.webquiz.model.Answer;
 import com.example.webquiz.model.Quiz;
 import com.example.webquiz.model.Result;
 import com.example.webquiz.repository.QuizRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -30,12 +32,26 @@ public class QuizService {
         return quizRepo.createQuiz(quiz);
     }
 
-    public Result solveQuiz(int quizId, int answer) {
+    public Result solveQuiz(int quizId, Answer answer) {
         Quiz quiz = quizRepo.getQuiz(quizId)
                 .orElseThrow(() -> new QuizNotFoundException("Quiz not found for id: " + quizId));
-        if (quiz.getAnswer() == answer) {
+
+        int[] quizAnswer = quiz.getAnswer();
+        int[] userAnswer = answer.getAnswer();
+
+        if (quizAnswer == null && userAnswer.length == 0) {
             return new Result(true, "Congratulations, you're right!");
         }
+
+        if (quizAnswer != null) {
+            Arrays.sort(quizAnswer);
+            Arrays.sort(userAnswer);
+
+            if (Arrays.equals(quizAnswer, userAnswer)) {
+                return new Result(true, "Congratulations, you're right!");
+            }
+        }
+
         return new Result(false, "Wrong answer! Please, try again.");
     }
 }
