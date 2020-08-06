21b77edcc984bc7ab6a7e22eb1ee07deac6e2841
ljurak <lucas.jurak@gmail.com>
2020-08-07 01:06:20 +0200

    Add custom error handler

---
+++ src/main/java/com/example/webquiz/controller/CustomExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.webquiz.controller;
+
+import com.example.webquiz.model.ApiError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.add("Field: " + fieldError.getField() + ", rejected value: " + fieldError.getRejectedValue());
+        }
+        ApiError error = new ApiError(HttpStatus.BAD_REQUEST, errors);
+        return handleExceptionInternal(ex, error, headers, error.getHttpStatus(), request);
+    }
+}
