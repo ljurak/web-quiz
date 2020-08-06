be0920b82b0cf2e5d14dd94373d88c9e0bf2dd44
ljurak <lucas.jurak@gmail.com>
2020-08-07 01:05:51 +0200

    Add dto for error response

---
+++ src/main/java/com/example/webquiz/model/ApiError.java
@@ -0,0 +1,25 @@
+package com.example.webquiz.model;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class ApiError {
+
+    private final HttpStatus httpStatus;
+
+    private final List<String> errors;
+
+    public ApiError(HttpStatus httpStatus, List<String> errors) {
+        this.httpStatus = httpStatus;
+        this.errors = errors;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
