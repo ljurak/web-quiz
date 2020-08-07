c432ff96fb6e75965ed814a70931f339055e6a14
ljurak <lucas.jurak@gmail.com>
2020-08-07 03:44:03 +0200

    Add entity property mappings

--- src/main/java/com/example/webquiz/model/Quiz.java
+++ src/main/java/com/example/webquiz/model/Quiz.java
@@ -2,41 +2,54 @@ package com.example.webquiz.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.List;
 
+@Entity
 public class Quiz {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private int id;
+    private Integer id;
 
     @NotBlank
-    private final String title;
+    private String title;
 
     @NotBlank
-    private final String text;
+    private String text;
 
+    @ElementCollection
+    @CollectionTable(name = "OPTIONS")
+    @Column(name = "OPTION")
     @NotEmpty
     @Size(min = 2)
-    private final String[] options;
+    private List<String> options;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private final int[] answer;
+    private int[] answer;
 
-    public Quiz(String title, String text, String[] options, int[] answer) {
+    public Quiz() {
+    }
+
+    public Quiz(String title, String text, List<String> options, int[] answer) {
         this.title = title;
         this.text = text;
         this.options = options;
         this.answer = answer;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getTitle() {
@@ -47,7 +60,7 @@ public class Quiz {
         return text;
     }
 
-    public String[] getOptions() {
+    public List<String> getOptions() {
         return options;
     }
 
