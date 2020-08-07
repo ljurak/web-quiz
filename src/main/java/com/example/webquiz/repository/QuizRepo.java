62afd592e3871c00c7f6245c90ee496706f92076
ljurak <lucas.jurak@gmail.com>
2020-08-07 03:44:58 +0200

    Change to spring data jpa interface

--- src/main/java/com/example/webquiz/repository/QuizRepo.java
+++ src/main/java/com/example/webquiz/repository/QuizRepo.java
@@ -1,32 +1,10 @@
 package com.example.webquiz.repository;
 
 import com.example.webquiz.model.Quiz;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 @Repository
-public class QuizRepo {
+public interface QuizRepo extends CrudRepository<Quiz, Integer> {
 
-    private int currentId = 0;
-
-    private Map<Integer, Quiz> quizzes = new HashMap<>();
-
-    public Optional<Quiz> getQuiz(int id) {
-        return Optional.ofNullable(quizzes.get(id));
-    }
-
-    public List<Quiz> getAllQuizzes() {
-        return new ArrayList<>(quizzes.values());
-    }
-
-    public Quiz createQuiz(Quiz quiz) {
-        quiz.setId(++currentId);
-        quizzes.put(quiz.getId(), quiz);
-        return quiz;
-    }
 }
