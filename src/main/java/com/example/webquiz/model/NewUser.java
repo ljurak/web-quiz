2ac438eb9153aa5a2707c3a5028dc9ac4c96419b
ljurak <lucas.jurak@gmail.com>
2020-08-11 03:16:46 +0200

    Add dto for registering new users

---
+++ src/main/java/com/example/webquiz/model/NewUser.java
@@ -0,0 +1,32 @@
+package com.example.webquiz.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class NewUser {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 5, max = 255)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
