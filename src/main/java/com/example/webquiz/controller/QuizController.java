5cc48da8c657dbea70e37326225627ee8019e4e6
ljurak <lucas.jurak@gmail.com>
2020-08-11 16:42:51 +0200

    Add endpoint for deleting quizzes

--- src/main/java/com/example/webquiz/controller/QuizController.java
+++ src/main/java/com/example/webquiz/controller/QuizController.java
@@ -4,13 +4,16 @@ import com.example.webquiz.model.Answer;
 import com.example.webquiz.model.Quiz;
 import com.example.webquiz.model.Result;
 import com.example.webquiz.service.QuizService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -41,8 +44,15 @@ public class QuizController {
         return quizService.createQuiz(quiz, author);
     }
 
-    @PostMapping(path = "/quizzes/{id}/solve")
+    @PostMapping("/quizzes/{id}/solve")
     public Result solveQuiz(@PathVariable int id, @RequestBody @Valid Answer answer) {
         return quizService.solveQuiz(id, answer);
+    }
+
+    @DeleteMapping("/quizzes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteQuiz(@PathVariable int id, Authentication authentication) {
+        String author = ((UserDetails) authentication.getPrincipal()).getUsername();
+        quizService.deleteQuiz(id, author);
     }
 }
