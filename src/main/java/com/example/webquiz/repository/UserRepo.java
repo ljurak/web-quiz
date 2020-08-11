7d0f978de9046e9b8ea41323dfa4b7637c0a4f49
ljurak <lucas.jurak@gmail.com>
2020-08-11 03:22:55 +0200

    Add repository for users

---
+++ src/main/java/com/example/webquiz/repository/UserRepo.java
@@ -0,0 +1,13 @@
+package com.example.webquiz.repository;
+
+import com.example.webquiz.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Integer> {
+
+    Optional<User> findUserByEmail(String email);
+}
