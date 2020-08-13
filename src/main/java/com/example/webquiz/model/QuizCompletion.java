7b18dd0f08789e8db30a5d98941d31e7886e6f52
ljurak <lucas.jurak@gmail.com>
2020-08-14 01:34:16 +0200

    Add QuizCompletion entity for storing quiz results

---
+++ src/main/java/com/example/webquiz/model/QuizCompletion.java
@@ -0,0 +1,56 @@
+package com.example.webquiz.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class QuizCompletion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "QUIZ_ID", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Quiz quiz;
+
+    private LocalDateTime completedAt;
+
+    public QuizCompletion() {
+    }
+
+    public QuizCompletion(User user, Quiz quiz) {
+        this.user = user;
+        this.quiz = quiz;
+        this.completedAt = LocalDateTime.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+}
