c6184bd550f8afaca19b1c33706a5bbd6e5da345
ljurak <lucas.jurak@gmail.com>
2020-08-17 00:30:02 +0200

    Remove jackson and bean validation annotations

--- src/main/java/com/example/webquiz/model/entity/Quiz.java
+++ src/main/java/com/example/webquiz/model/entity/Quiz.java
@@ -1,8 +1,5 @@
 package com.example.webquiz.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -13,9 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -23,28 +17,21 @@ public class Quiz {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer id;
 
-    @NotBlank
     private String title;
 
-    @NotBlank
     private String text;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID", nullable = false)
-    @JsonIgnore
     private User author;
 
     @ElementCollection
     @CollectionTable(name = "OPTIONS")
     @Column(name = "OPTION")
-    @NotEmpty
-    @Size(min = 2)
     private List<String> options;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private int[] answer;
 
     public Quiz() {
@@ -72,10 +59,6 @@ public class Quiz {
 
     public User getAuthor() {
         return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
     }
 
     public List<String> getOptions() {
